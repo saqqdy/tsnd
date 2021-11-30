71d21f66b3da7ff48bd5f7b9ea9f9b0026fa7114
saqqdy <saqqdy@qq.com>
2021-11-30 09:11:54 +0800

    1.0.0

--- src/cjs.ts
+++ src/cjs.ts
@@ -4,6 +4,7 @@ const register = require.resolve('esbuild-register')
 const { sync: spawnSync } = require('cross-spawn')
 const argv = process.argv.slice(2)
 
+// 执行程序
 const program = spawnSync('node', ['-r', register, ...argv], {
     stdio: 'inherit',
     shell: process.platform === 'win32'
--- src/esm.ts
+++ src/esm.ts
@@ -5,6 +5,7 @@ import { resolve as importResolve } from 'import-meta-resolve'
 const argv = process.argv.slice(2)
 
 importResolve('esbuild-node-loader', import.meta.url).then(path => {
+    // 执行程序
     const program = spawnSync(
         'node',
         ['--experimental-loader', path, ...argv],
