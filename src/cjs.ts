2cc77ad33c885aec5b0330306a50860d5b1bf9ab
saqqdy <saqqdy@qq.com>
2021-12-11 13:57:41 +0800

    fix argvs

--- src/cjs.ts
+++ src/cjs.ts
@@ -2,8 +2,13 @@
 
 const register = require.resolve('esbuild-register')
 const { sync: spawnSync } = require('cross-spawn')
-const argv = process.argv.slice(2)
-
+const argv = process.argv
+    .slice(2)
+    .map((item, i) =>
+        i === 0 || item.indexOf('-') === 0 || /^\w+$/.test(item)
+            ? item
+            : '"' + item + '"'
+    )
 // 执行程序
 const program = spawnSync('node', ['-r', register, ...argv], {
     stdio: 'inherit',
--- src/esm.ts
+++ src/esm.ts
@@ -2,8 +2,13 @@
 
 import { sync as spawnSync } from 'cross-spawn'
 import { resolve as importResolve } from 'import-meta-resolve'
-const argv = process.argv.slice(2)
-
+const argv = process.argv
+    .slice(2)
+    .map((item, i) =>
+        i === 0 || item.indexOf('-') === 0 || /^\w+$/.test(item)
+            ? item
+            : '"' + item + '"'
+    )
 importResolve('esbuild-node-loader', import.meta.url).then(path => {
     // 执行程序
     const program = spawnSync(
